1f587add04ba9da1cfafab8e956793623fd29525
Bruno Tougeiro <b.tougeiro@icloud.com>
2016-07-21 02:13:19 -0300

    atualizado

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# arquivo de log
-log="$(date +%d%m%Y)-install.log"
-
 # verifica root
 if [ $(id -u) -ne 0 ]; then
   echo "Voce precisa ser root para continuar"
@@ -19,7 +16,6 @@ fi
 # boas vindas
 echo
 echo "Bem-vindo ao sistema de deploy do ambiente Node.js"
-echo "Para maiores informacoes consulte o arquivo de log gerado durante o processo de deploy"
 echo
 
 # iniciando procedimento
@@ -36,20 +32,20 @@ fi
 
 # atualizacao do sistema
 echo "Atualizando o sistema, aguarde..."
-apt-get update >> $log
-apt-get upgrade -y >> $log 
+apt-get update
+apt-get upgrade -y
 
 # instalacao dos pacotes do docker
 echo "Instalando docker e suas dependencias"
-apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D >> $log
-echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list >> $log
-apt-get update >> $log
-apt-get install -y docker-engine >> $log
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get install -y docker-engine
 
 # criando imagem usando o Dockerfile
 echo "Criando imagem do app node.js"
 cd app
-docker build -t node.js/node-app . >> $log
+docker build -t node.js/node-app .
 
 # iniciando a aplicacao node.js
-docker run -p 3000:3000 -d node.js/node-app >> $log
+docker run -p 3000:3000 -d node.js/node-app
