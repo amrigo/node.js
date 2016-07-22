58ad3286384fef16de3c694e0d90b9b198c92895
Bruno Tougeiro <b.tougeiro@icloud.com>
2016-07-22 00:42:34 -0300

    new cluster file

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 var cluster = require('cluster');
-var http = require('http');
-var numCPUs = require('os').cpus().length;
-var port = 3000
+var numWorkers = require('os').cpus().length;
 
-if (numCPUs > 0 || numCPUs <= 32)
-{
-  if(cluster.isMaster)
-  {
-    for (var i = 0; i < numCPUs; i++)
-    {
-      cluster.fork();
+if(cluster.isMaster && numWorkers > 0 || numWorkers <= 32) {
+    console.log('Master cluster setting up ' + numWorkers + ' workers...');
+
+    for(var i = 0; i < numWorkers; i++) {
+        cluster.fork();
     }
-  }
-  else
-  {
-    http.createServer(function(req, res)
-    {
-      res.writeHead(200);
-      res.send('Bem-vindo a aplicacao de teste Node.JS');
-      res.send('Este e o processo ' + process.pid + ' em execução');
-    }).listen(port);
-  }
+
+    cluster.on('online', function(worker) {
+        console.log('Worker ' + worker.process.pid + ' is online');
+    });
+
+    cluster.on('exit', function(worker, code, signal) {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
+    });
+} else {
+    var app = require('express')();
+    app.all('/*', function(req, res) {res.send('process ' + process.pid + ' says hello!').end();})
+
+    var server = app.listen(3000, function() {
+        console.log('Process ' + process.pid + ' is listening to all incoming requests');
+    });
 }
